a876a2ea372c19a4c9c19baa7d797548542389e5
Architect <134674732+harsh-a-parihar@users.noreply.github.com>
2024-02-28 13:35:36 +0530

    Update _3.Numpy_indexing.py

--- _3.Numpy_indexing.py
+++ _3.Numpy_indexing.py
@@ -5,7 +5,7 @@ _array1 = np.array([11, 22, 33, 44, 55])     # create 1-d array
 _array2 = np.array([[1, 2, 3], [4, 5, 6]])      # create 2-d array
 _array3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])   # 3-d create array
 
-print(_array1[2])   # index->33
-print(_array1[0])    # index->11
-print(_array2[0:1])     # index->from 0 to 1(excluded)
-print(_array3[0:1:2])   # index->from 0 to 1 but steps=2
+print(_array1[2])   # number at index->33
+print(_array1[0])    # number at index->11
+print(_array2[0:1])     # number at index->from 0 to 1(excluded)
+print(_array3[0:1:2])   # number at index->from 0 to 1 but steps=2
